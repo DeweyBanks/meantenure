1f865326c8b62f8e99d263a94fac1f27b221a0eb
Dewey Banks <DeweyBanks@gmail.com>
2017-06-27 13:46:51 -0400

    adds user auth and passport

--- server.js
+++ server.js
@@ -1,70 +1,18 @@
 var express = require('express');
-var stylus = require('stylus');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path) {
-  return stylus(str).set('filename', path);
-}
+var config = require('./server/config/config')[env];
 
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(stylus.middleware(
-  {
-    src: __dirname + '/public',
-    compile: compile
-  }
-));
-app.use(express.static(__dirname + '/public'));
+require('./server/config/express')(app, config);
 
+require('./server/config/mongoose')(config);
 
+require('./server/config/passport')();
 
-if(env === 'development') {
-  mongoose.connect('mongodb://localhost:27017/tenureed');
-} else {
-  mongoose.connect('mongodb://deweyb:tenureed@ds135532.mlab.com:35532/heroku_jzshb376');
-}
+require('./server/config/routes')(app);
 
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-  console.log('tenureed db opened:: ');
-});
-
-
-
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage = new Message({message: 'Hello mongoDB here'});
-mongoMessage.save(function(err, doc) {
-    Message.findOne().exec(function(err, messageDoc) {
-        mongoMessage = messageDoc.message;
-    });
-});
-
-
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-});
-
-
-// the * says to serve index file no matter what the request. this will allow the client side routes to take over
-app.get('*', function(req, res) {
-  res.render('index', {
-    mongoMessage: mongoMessage
-  });
-});
-
-
-var port = process.env.PORT || 3030;
-app.listen(port);
-console.log('Listening on port ' + port + '...');
+app.listen(config.port);
+console.log('Listening on port ' + config.port + '...');
